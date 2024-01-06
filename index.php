91bbc7fe3648cce4a8df13d98c53739794587832
Corrupted <devndom03@gmail.com>
2024-01-05 21:41:56 -0800

    Hi

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title> IPA Signer </title>
+</head>
+
+<body>
+
+<form action="files.php" method="post">
+ <input type="text" name="ipaid" placeholder="iPA">
+ <button>Sign</button>
+</form>
+
+</body>
+<html>
